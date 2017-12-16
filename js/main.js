465c8ec3fdd77e9374720f962ce8ce7b0367c689
Jason Kim <jason.kim1002578@gmail.com>
2017-12-15 20:23:26 -0500

    add finished index page

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ var fadeIn = () => {
 
 window.onload = fadeIn
 
+function getMousePos(evt) {
+    var doc = document.documentElement || document.body;
+    var pos = {
+        x: evt ? evt.pageX : window.event.clientX + doc.scrollLeft - doc.clientLeft,
+        y: evt ? evt.pageY : window.event.clientY + doc.scrollTop - doc.clientTop
+    };
+    return pos;
+}
+
+document.onmousemove = moveMouse;
+
+function moveMouse(evt) {
+    var pos = getMousePos(evt),
+		followMouse = document.getElementById("_followMouse");
+    followMouse.style.backgroundPosition = pos.x + "px " + pos.y + "px";
+}
