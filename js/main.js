258c6a2b082c66ae8c25101b13d12b199104aeea
Jason Kim <jasonkim@mac-jasonkim-c02f1b93md6n.attlocal.net>
2022-01-07 01:32:06 -0800

    new website 2022

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var body = document.getElementsByTagName('body')[0];
-
-var fadeIn = () => {
-  body.classList.add('fade-in');
-}
-
-window.onload = fadeIn
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+var body = document.getElementsByTagName("body")[0];
+
+var fadeIn = () => {
+  body.classList.add("fade-in");
+};
+
+window.onload = fadeIn;
