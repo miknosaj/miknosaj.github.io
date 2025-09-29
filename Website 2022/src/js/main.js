2455c2a66e140da3d3bfc22cc009cda3ea58c890
Jason Kim <jajasonkim@gmail.com>
2025-09-29 01:34:21 -0400

    update changes

---
+++ Website 2022/src/js/main.js
@@ -0,0 +1,8 @@
+var body = document.getElementsByTagName("body")[0];
+
+var init = () => {
+  body.classList.add("fade-in");
+  body.classList.add("slide-up");
+};
+
+window.onload = init;
--- features/portfolio/utils/dimming.ts
+++
@@ -1,2 +0,0 @@
-export const getDimmedClasses = (shouldDim: boolean) => (shouldDim ? 'portfolio-dimmed' : '');
-export const getDimmedCursor = (shouldDim: boolean) => (shouldDim ? 'pointer' : 'default');
